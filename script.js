cb418c2828a3136eb2926f7e0f5249f1804c85e0
mansur-kl <shaikmansur625@gmail.com>
2025-09-08 12:34:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const a = "ambani"
+const b = "31351"
+const c = a+b
+document.writeln("Output is: " ,c)
+
+const d = 6
+const e = 7
+const f = "mukash"
+const g = d+e+f
+document.writeln("Output is: " ,g)
+
+let h = 10;
+let i = 20;
+let j = h**3;
+let k = i*h;
+document.writeln("Output is: " ,h)
+document.writeln("Output is: " ,i)
+document.writeln("Output is: " ,j)
+document.writeln("Output is: " ,k)
